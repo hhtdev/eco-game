06ef672666f70f2dec995bebc6557a5a54560990
Hugo Herault <hugo.herault-ext@francetravail.fr>
2024-11-06 11:33:32 +0100

    WIP init

---
+++ script.js
@@ -0,0 +1,11 @@
+const Reponses = {
+  reponse1: 'réponse 1',
+  reponse2: 'réponse 2',
+  reponse3: 'réponse 3',
+};
+
+export default Reponses;
+
+function displayReponse(reponseBtn) {
+  console.log(reponses[reponseBtn]);
+}
