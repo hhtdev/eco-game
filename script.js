afe2d1821c43153c696faefc2aca1a1bae34f3a8
Raphaël <rchaudre@keepcod.fr>
2024-11-06 12:28:11 +0100

    update list with real answers

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 const Reponses = {
-  reponse1: 'réponse 1',
-  reponse2: 'réponse 2',
-  reponse3: 'réponse 3',
-  reponse4: 'réponse 4',
-  reponse5: 'réponse 5',
-  reponse6: 'réponse 6',
-  reponse7: 'réponse 7',
-  reponse8: 'réponse 8',
+  reponse1: 'SSR',
+  reponse2: 'Cache front/back',
+  reponse3: 'Lazy loading',
+  reponse4: 'Utiliser un framework modulaire',
+  reponse5: 'Batch plutôt que unitaire',
+  reponse6: 'Traitement asynchrone',
+  reponse7: 'Image AVIF/WEBP',
+  reponse8: 'Compression/minification CSS',
 };
 
 function displayReponse(reponseNumber) {
   for (const reponse in Reponses) {
     if (reponse === reponseNumber) {
-      console.log(Reponses[reponse]);
       document.getElementById(reponse).innerHTML = Reponses[reponse];
     }
   }
