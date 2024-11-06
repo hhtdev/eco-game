8eb2e88496cacb34045ed26b500a1b956e7db943
Hugo Herault <hugo.herault-ext@francetravail.fr>
2024-11-06 11:45:26 +0100

    logic done

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ const Reponses = {
   reponse1: 'réponse 1',
   reponse2: 'réponse 2',
   reponse3: 'réponse 3',
+  reponse4: 'réponse 4',
+  reponse5: 'réponse 5',
+  reponse6: 'réponse 6',
+  reponse7: 'réponse 7',
+  reponse8: 'réponse 8',
 };
 
 function displayReponse(reponseNumber) {
   for (const reponse in Reponses) {
     if (reponse === reponseNumber) {
       console.log(Reponses[reponse]);
+      document.getElementById(reponse).innerHTML = Reponses[reponse];
     }
   }
 }
